87f614d979377d5bef6ce97e4014c9c7e9ff3416
imsukanta <sukantamalik2002@gmail.com>
2025-03-12 16:28:01 +0530

    add command

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
 from flask_migrate import Migrate
 from flask_restful import Api
-import os
+
 class Base(DeclarativeBase):
     pass
 db=SQLAlchemy(model_class=Base)
