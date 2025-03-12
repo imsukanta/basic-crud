b2f750129493694595cf3336867368aacd456be0
imsukanta <sukantamalik2002@gmail.com>
2025-03-12 16:30:14 +0530

    new command

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-flask db init
-flask db migrate -m "Initial migration"
-flask db upgrade
+flask --app flaskr db init
+flask --app flaskr db migrate -m "Initial migration"
+flask --app flaskr db upgrade
 gunicorn -w 4 -b 0.0.0.0:$PORT "flaskr:create_app()"
